c8c7f2f4bf3a8fa4115dd8f76d8a1ee8aaa8812c
James Enietan <enietanjamie@gmail.com>
2025-08-02 10:51:12 +0100

    Simple deploy: Sat 02/08/2025 10:51:11.88

--- crysgarage-frontend/vite.config.ts
+++ crysgarage-frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-    allowedHosts: ['localhost', '127.0.0.1', 'crysgarage.studio', 'www.crysgarage.studio', '209.74.80.162', 'frontend'],
+    allowedHosts: 'all',
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
